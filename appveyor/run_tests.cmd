27a271859ce93d1c15c00986c99be41ae0077ec1
Keith Erskine <toastie604@gmail.com>
2020-02-01 07:55:08 -0600

    Update run_tests.cmd

--- appveyor/run_tests.cmd
+++ appveyor/run_tests.cmd
@@ -3,9 +3,9 @@ SET OVERALL_RESULT=0
 
 IF "%APVYR_RUN_TESTS%" == "true" (
   ECHO Running MS SQL Server tests
-  sqlcmd -S "(local)\%MSSQL_NAME%" -U sa -P "Password12!" -Q "SELECT @@VERSION"^
-  && sqlcmd -S "(local)\%MSSQL_NAME%" -U sa -P "Password12!" -Q "CREATE DATABASE test_db"^
-  && "%PYTHON%\python" "%TESTS_DIR%\sqlservertests.py" "%MSSQL_CONN%"
+  sqlcmd -S "(local)\%MSSQL_NAME%" -U sa -P "Password12!" -Q "SELECT @@VERSION"
+  sqlcmd -S "(local)\%MSSQL_NAME%" -U sa -P "Password12!" -Q "CREATE DATABASE test_db"
+  "%PYTHON%\python" "%TESTS_DIR%\sqlservertests.py" "%MSSQL_CONN%"
   IF %ERRORLEVEL% NEQ 0 SET OVERALL_RESULT=1
 ) ELSE (
   ECHO Skipping MS SQL Server tests
