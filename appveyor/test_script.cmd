30f9afc2590f9654c65674fb2b5183d92cc164af
Keith Erskine <toastie604@gmail.com>
2020-02-02 07:26:21 -0600

    multiple MSSQL drivers

--- appveyor/test_script.cmd
+++ appveyor/test_script.cmd
@@ -7,40 +7,119 @@ SET OVERALL_RESULT=0
 
 IF NOT "%APVYR_RUN_TESTS%" == "true" (
   ECHO Skipping all the unit tests
-  EXIT /B %OVERALL_RESULT%
+  GOTO :end
 )
 
+
+ECHO ############################################################
+ECHO # MS SQL Server
+ECHO ############################################################
 IF "%APVYR_RUN_MSSQL_TESTS%" == "true" (
   ECHO Running the MS SQL Server unit tests
-  sqlcmd -S "%MSSQL_INSTANCE%" -U sa -P "Password12!" -Q "SELECT @@VERSION"^
-  && sqlcmd -S "%MSSQL_INSTANCE%" -U sa -P "Password12!" -Q "CREATE DATABASE test_db"^
-  && "%PYTHON%\python" "%TESTS_DIR%\sqlservertests.py" "%MSSQL_CONN%"
-  IF ERRORLEVEL 1 SET OVERALL_RESULT=1
 ) ELSE (
   ECHO Skipping the MS SQL Server unit tests
+  GOTO :postgresql
+)
+ECHO Get MS SQL Server version
+sqlcmd -S "%MSSQL_INSTANCE%" -U sa -P "Password12!" -Q "SELECT @@VERSION"
+IF ERRORLEVEL 1 (
+  ECHO ERROR: Could not connect to instance %MSSQL_INSTANCE%
+  GOTO :postgresql
+)
+ECHO Create test database
+sqlcmd -S "%MSSQL_INSTANCE%" -U sa -P "Password12!" -Q "CREATE DATABASE test_db"
+IF ERRORLEVEL 1 (
+  ECHO ERROR: Could not create the test database
+  GOTO :postgresql
 )
 
-REM TODO? Should I create a separate database for the tests?
+:mssql1
+SET CONN_STR=Driver={SQL Server Native Client 10.0};Server=%MSSQL_INSTANCE%;Database=test_db;UID=sa;PWD=Password12!;
+ECHO Connection string (1): %CONN_STR%
+"%PYTHON%\python" -c "import pyodbc; pyodbc.connect(%CONN_STR%).close()"
+IF ERRORLEVEL 1 (
+  ECHO INFO: Could not connect using the connection string
+  GOTO :mssql2
+)
+"%PYTHON%\python" "%TESTS_DIR%\sqlservertests.py" "%CONN_STR%"
+IF ERRORLEVEL 1 SET OVERALL_RESULT=1
+
+:mssql2
+SET CONN_STR=Driver={SQL Server Native Client 11.0};Server=%MSSQL_INSTANCE%;Database=test_db;UID=sa;PWD=Password12!;
+ECHO Connection string (2): %CONN_STR%
+"%PYTHON%\python" -c "import pyodbc; pyodbc.connect(%CONN_STR%).close()"
+IF ERRORLEVEL 1 (
+  ECHO INFO: Could not connect using the connection string
+  GOTO :mssql3
+)
+"%PYTHON%\python" "%TESTS_DIR%\sqlservertests.py" "%CONN_STR%"
+IF ERRORLEVEL 1 SET OVERALL_RESULT=1
+
+:mssql3
+SET CONN_STR=Driver={ODBC Driver 11 for SQL Server};Server=%MSSQL_INSTANCE%;Database=test_db;UID=sa;PWD=Password12!;
+ECHO Connection string (3): %CONN_STR%
+"%PYTHON%\python" -c "import pyodbc; pyodbc.connect(%CONN_STR%).close()"
+IF ERRORLEVEL 1 (
+  ECHO INFO: Could not connect using the connection string
+  GOTO :mssql4
+)
+"%PYTHON%\python" "%TESTS_DIR%\sqlservertests.py" "%CONN_STR%"
+IF ERRORLEVEL 1 SET OVERALL_RESULT=1
+
+:mssql4
+SET CONN_STR=Driver={ODBC Driver 13 for SQL Server};Server=%MSSQL_INSTANCE%;Database=test_db;UID=sa;PWD=Password12!;
+ECHO Connection string (4): %CONN_STR%
+"%PYTHON%\python" -c "import pyodbc; pyodbc.connect(%CONN_STR%).close()"
+IF ERRORLEVEL 1 (
+  ECHO INFO: Could not connect using the connection string
+  GOTO :postgresql
+)
+"%PYTHON%\python" "%TESTS_DIR%\sqlservertests.py" "%CONN_STR%"
+IF ERRORLEVEL 1 SET OVERALL_RESULT=1
+
+
+:postgresql
+REM TODO: create a separate database for the tests?
+ECHO ############################################################
+ECHO # PostgreSQL
+ECHO ############################################################
 IF "%APVYR_RUN_POSTGRES_TESTS%" == "true" (
   ECHO Running the PostgreSQL unit tests
-  "C:\Program Files\PostgreSQL\9.6\bin\postgres" --version^
-  && "%PYTHON%\python" "%TESTS_DIR%\pgtests.py" "%POSTGRES_CONN%"
+  REM ECHO Get psql version
+  REM "C:\Program Files\PostgreSQL\9.6\bin\postgres" --version
+  ECHO Get PostgreSQL version
+  SET PGPASSWORD=Password12!
+  "%POSTGRES_PATH%\bin\psql" -U postgres -d postgres -c "SELECT version()"
+  SET CONN_STR=Driver={PostgreSQL Unicode(x64)};Server=localhost;Port=5432;Database=postgres;Uid=postgres;Pwd=Password12!;
+  ECHO Connection string: %CONN_STR%
+  "%PYTHON%\python" "%TESTS_DIR%\pgtests.py" "%CONN_STR%"
   IF ERRORLEVEL 1 SET OVERALL_RESULT=1
 ) ELSE (
   ECHO Skipping the PostgreSQL unit tests
 )
 
-REM TODO? Should I create a separate database for the tests?  (with the right collation)
-REM https://dev.mysql.com/doc/refman/5.7/en/charset-charsets.html
-REM e.g. CREATE DATABASE test_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+:mysql
+REM TODO: create a separate database for the tests?  (with the right collation)
+REM       https://dev.mysql.com/doc/refman/5.7/en/charset-charsets.html
+REM       e.g. CREATE DATABASE test_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+ECHO ############################################################
+ECHO # MySQL
+ECHO ############################################################
 IF "%APVYR_RUN_MYSQL_TESTS%" == "true" (
   ECHO Running the MySQL unit tests
-  "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql" --version^
-  && "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql" -u root -pPassword12! -e "SHOW DATABASES;"^
-  && "%PYTHON%\python" "%TESTS_DIR%\mysqltests.py" "%MYSQL_CONN%"
+  ECHO Get MySQL version
+  "%MYSQL_PATH%\bin\mysql" --version
+  "%MYSQL_PATH%\bin\mysql" -u root -pPassword12! -e "SELECT VERSION()"
+  "%MYSQL_PATH%\bin\mysql" -u root -pPassword12! -e "STATUS"
+  "%MYSQL_PATH%\bin\mysql" -u root -pPassword12! -e "SHOW DATABASES"
+  SET CONN_STR=Driver={MySQL ODBC 5.3 ANSI Driver};Charset=utf8mb4;Server=localhost;Port=3306;Database=mysql;Uid=root;Pwd=Password12!;
+  ECHO Connection string: %CONN_STR%
+  "%PYTHON%\python" "%TESTS_DIR%\mysqltests.py" "%CONN_STR%"
   IF ERRORLEVEL 1 SET OVERALL_RESULT=1
 ) ELSE (
   ECHO Skipping the MySQL unit tests
 )
 
+:end
 EXIT /B %OVERALL_RESULT%
