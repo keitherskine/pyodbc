9ed89a65f3255863dac9bcb462b38e15be6b4140
Keith Erskine <toastie604@gmail.com>
2020-05-28 06:02:36 -0500

    Update keith.py

--- tests3/keith.py
+++ tests3/keith.py
@@ -9,17 +9,21 @@ cnxn.maxwrite = 1024 * 1024 * 1024
 crsr = cnxn.cursor()
 
 crsr.execute("DROP TABLE IF EXISTS t1")
-crsr.execute("CREATE TABLE t1(s1 varchar(100), s2 varchar(100))")
+crsr.execute("CREATE TABLE t1(id int, s1 varchar(100), s2 varchar(100))")
 
 v = "x \U0001F31C z"
-crsr.execute(r"insert into t1 values (U&'x \+01F31C z', '{}')".format(v))
+v2 = '我的'
+crsr.execute(r"insert into t1 values (1, U&'x \+01F31C z', '{}')".format(v))
+crsr.execute(r"insert into t1 values (2, U&'\6211\7684', '{}')".format(v))
 
-rows = crsr.execute("select * from t1").fetchall()
+rows = crsr.execute("select * from t1 order by id").fetchall()
 for row in rows:
     print('row:', row)
 
 print(v == rows[0][0])
 print(v == rows[0][1])
+print(v2 == rows[1][0])
+print(v2 == rows[1][1])
 
 crsr.close()
 cnxn.close()
