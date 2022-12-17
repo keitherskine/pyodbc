7ec18f6a24968018de0981c3a5a718a2f2221819
Keith Erskine <toastie604@gmail.com>
2022-12-17 12:55:33 -0600

    orig drivers and appveyor py versions 2

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -237,7 +237,7 @@ if ($python_arch -eq "64") {
             -driver_name "MySQL ODBC 8.0 ANSI Driver" `
             -driver_bitness "32-bit" `
             -driver_url "https://dev.mysql.com/get/Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.19-win32.msi" `
-            -msifile_path "$cache_dir\mysql-connector-odbc-8.0.19-win32.msi";;
+            -msifile_path "$cache_dir\mysql-connector-odbc-8.0.19-win32.msi";
     }
 } else {
     Write-Output "ERROR: Unexpected Python architecture:"
